1d56bb0d445fbbd848ed1a5ada5fc833ff2df093
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-02-14 19:08:18 -0300

    Added main content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Images from './components/Images';
+import ScoreBoard from './components/ScoreBoard';
 import './styles/styles.css';
 
 export default function App() {
+  const [currScore, setCurrScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
-    <div className="App">App</div>
+    <main className="flex">
+      <ScoreBoard
+        currScore={currScore}
+        bestCore={bestScore}
+      />
+      <Images currScore={currScore} setCurrScore={setCurrScore} setBestScore={setBestScore} />
+    </main>
   );
 }
