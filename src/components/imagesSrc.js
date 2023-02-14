72fbf2549f44ec23bca2acb649c903c2547e09cc
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-02-14 19:06:48 -0300

    Added images source

---
+++ src/components/imagesSrc.js
@@ -0,0 +1,25 @@
+import yotsugi from '../images/301226.jpg';
+import mayoi from '../images/222381.jpg';
+import shinobu from '../images/295362.jpg';
+import araragi from '../images/148437.jpg';
+import sodachi from '../images/300557.jpg';
+import tsukihi from '../images/149433.jpg';
+import karen from '../images/289884.jpg';
+import kanbaru from '../images/222449.jpg';
+import kaiki from '../images/155489.jpg';
+import senjouhara from '../images/299586.jpg';
+
+const imagesSrc = [
+  { src: yotsugi, alt: 'Ononki Yotsugi' },
+  { src: mayoi, alt: 'Hachikuji Mayoi' },
+  { src: shinobu, alt: 'Oshino Shinobu' },
+  { src: araragi, alt: 'Araragi Koyomi' },
+  { src: sodachi, alt: 'Oikura Sodachi' },
+  { src: tsukihi, alt: 'Araragi Tsukihi' },
+  { src: karen, alt: 'Araragi Karen' },
+  { src: kanbaru, alt: 'Kanbaru Suruga' },
+  { src: kaiki, alt: 'Kaiki Deishuu' },
+  { src: senjouhara, alt: 'Senjouhara Hitagi' },
+];
+
+export default imagesSrc;
